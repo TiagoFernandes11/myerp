d83fea61b10c63f8bd6b8d6fb04c17eb121429e4
Tiago Fernandes Ribeiro <tiago.ribeiro@petzpartner.com.br>
2025-05-23 20:15:36 -0300

    layouts e tela de login basicas

---
+++ src/main/java/br/erp/myerp/frontend/src/app/login/page.tsx
@@ -0,0 +1,16 @@
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="bg-gray-900 pt-20 pb-20 pr-10 pl-10">
+        <h1 className="mb-2">MyERP</h1>
+    <form className="flex flex-col" method="POST" action={"http://localhost:8080/api/login"}>
+          <label className="mb-2">Username</label>
+          <input className="mb-4" type="text" name="username" id="username" />
+          <label className="mb-2">Password</label>
+          <input className="mb-4" type="text" name="password" id="password" />
+          <input className="bg-blue-900 mt-5" type="submit" placeholder="Login" />
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/main/java/br/erp/myerp/frontend/src/components/sidebar.tsx
+++ src/main/java/br/erp/myerp/frontend/src/components/sidebar.tsx
@@ -4,8 +4,12 @@ export default function SideBar() {
     <aside className="w-auto h-screen bg-gray-900 text-white p-6 shadow-lg">
       <h1 className="text-2xl font-bold mb-6">Sidebar</h1>
       <ul className="space-y-4">
-        <li className="hover:text-blue-400 cursor-pointer">Dashboard</li>
-        <li className="hover:text-blue-400 cursor-pointer">Settings</li>
+        <li className="hover:text-blue-400 cursor-pointer">
+            <a href="/dashboard">Dashboard</a>
+        </li>
+        <li className="hover:text-blue-400 cursor-pointer">
+            <a href="/settings">Settings</a>
+        </li>
         <li className="hover:text-blue-400 cursor-pointer">Profile</li>
         <li className="hover:text-blue-400 cursor-pointer">Logout</li>
       </ul>
